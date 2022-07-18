fc3620166b849d550a014385700612b8c68a7c98
k.okayama <k.okayama@gemcook.com>
2022-07-18 15:27:42 +0900

    ROM-4 linariaの設定をする。

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withLinaria = require('next-linaria');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const linariaOption = {};
+
+module.exports = withLinaria({
+  ...nextConfig,
+  linaria: linariaOption,
+});
