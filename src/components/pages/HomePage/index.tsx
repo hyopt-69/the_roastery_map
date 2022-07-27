73559af4930b14fd9bb43f0425d10f94049acdd2
k.okayama <k.okayama@gemcook.com>
2022-07-28 01:14:50 +0900

    chore

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { Icon } from '@/components/parts/Icon';
-import { Title } from '@/components/parts/Text/Title';
-import Home from '@/pages';
+import { Title } from '@/components/parts/Title';
 
 export const HomePage: React.FC = () => {
   return (
     <>
-      <Title>Home</Title>
+      <Title size="l">Home</Title>
+      <Icon pattern="EXAMPLE" />
     </>
   );
 };
