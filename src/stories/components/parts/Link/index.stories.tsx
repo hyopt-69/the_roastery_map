32539bf01b06a8b7795e0771b23a624ed49bd3c8
k.okayama <k.okayama@gemcook.com>
2022-08-12 19:42:19 +0900

    ROM-74 LinkButtonコンポーネントの実装

---
+++ src/stories/components/parts/Link/index.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+import { Link } from '@/components/parts/Link';
+import { DUMMY_URL } from '@/stories/dummys/url';
+
+export default {
+  component: Link,
+} as ComponentMeta<typeof Link>;
+
+export const Index: ComponentStoryObj<typeof Link> = {
+  args: {
+    children: 'Example',
+    href: DUMMY_URL,
+    size: 'm',
+    mqSizes: ['m', 'xxl'],
+    weight: 'normal',
+    target: '_blank',
+    color: 'mirage',
+  },
+};
