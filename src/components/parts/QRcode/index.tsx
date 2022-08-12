437c88ea9124c7b3f921d23caf2730fdf75a0924
k.okayama <k.okayama@gemcook.com>
2022-08-12 10:58:13 +0900

    QRcodeの修正

--- src/components/parts/QRcode/index.tsx
+++ src/components/parts/QRcode/index.tsx
@@ -1,26 +1,34 @@
 import { useQRCode } from 'next-qrcode';
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 
 import { colors } from '@/app/theme/colors';
 
-import { getStyles, QRCodeSize } from './styles';
+import { styles } from './styles';
 
 type Props = {
   url: string;
-  size: QRCodeSize;
 };
 
-export const QRcode: React.FC<Props> = ({ url, size }) => {
+export const QRcode: React.FC<Props> = ({ url }) => {
+  const wrapperRef = useRef<React.ElementRef<'div'>>(null);
+  const [qrWidth, setQrWidth] = useState(0);
   const { Image } = useQRCode();
 
+  useLayoutEffect(() => {
+    if (wrapperRef.current) {
+      setQrWidth(wrapperRef.current.getBoundingClientRect().width);
+    }
+  }, []);
+
   return (
-    <div css={getStyles(size)}>
+    <div ref={wrapperRef} css={styles.container}>
       <Image
         text={url}
         options={{
           type: 'image/webp',
           level: 'M',
           margin: 0,
+          width: qrWidth,
           color: {
             dark: colors.mirage,
             light: colors.white,
--- src/components/parts/QRcode/styles.ts
+++ src/components/parts/QRcode/styles.ts
@@ -1,15 +1,7 @@
 import { css } from '@emotion/react';
 
-export type QRCodeSize = 's' | 'm' | 'l' | 'fill';
-
-const QRCODE_SIZES: Record<QRCodeSize, number | '100%'> = {
-  s: 24,
-  m: 36,
-  l: 48,
-  fill: '100%',
+export const styles = {
+  container: css({
+    width: '100%',
+  }),
 };
-
-export const getStyles = (size: QRCodeSize) =>
-  css({
-    width: QRCODE_SIZES[size],
-  });
