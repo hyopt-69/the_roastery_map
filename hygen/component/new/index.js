4c40cad6d4c8198b44ec392313c4db8d86e5d2b3
k.okayama <k.okayama@gemcook.com>
2022-08-17 21:51:34 +0900

    ROM-90 ディレクトリの整理

--- hygen/component/new/index.js
+++ hygen/component/new/index.js
@@ -1,39 +1,42 @@
-import path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
-export function prompt({ inquirer }) {
-  const questions = [
-    {
-      message: `What's the component name?`,
-      name: 'name',
-      type: 'input',
-      validate: (input) => {
-        const isNull = input === '';
-        const isLowerCase = input.toLowerCase() === input;
-        return !isNull && !isLowerCase;
+module.exports = {
+  prompt: ({ inquirer }) => {
+    const questions = [
+      {
+        message: `What's the component name?`,
+        name: 'name',
+        type: 'input',
+        validate: (input) => {
+          const isNull = input === '';
+          const isLowerCase = input.toLowerCase() === input;
+          return !isNull && !isLowerCase;
+        },
       },
-    },
-    {
-      choices: ['parts', 'patterns', 'layouts'],
-      message: `What's the atomic group ?`,
-      name: 'atomicGroup',
-      type: 'select',
-    },
-    {
-      message: `What's the component group?`,
-      name: 'componentGroup',
-      type: 'input',
-      validate: (input) => {
-        const isNull = input === '';
-        const isLowerCase = input.toLowerCase() === input;
-        return isNull || !isLowerCase;
+      {
+        choices: ['parts', 'patterns', 'layouts'],
+        message: `What's the atomic group ?`,
+        name: 'atomicGroup',
+        type: 'select',
       },
-    },
-  ];
+      {
+        message: `What's the component group?`,
+        name: 'componentGroup',
+        type: 'input',
+        validate: (input) => {
+          const isNull = input === '';
+          const isLowerCase = input.toLowerCase() === input;
+          return isNull || isLowerCase;
+        },
+      },
+    ];
 
-  return inquirer
-    .prompt(questions)
-    .then(({ name, atomicGroup, componentGroup }) => ({
-      directory: path.join(atomicGroup, componentGroup),
-      name,
-    }));
-}
+    return inquirer
+      .prompt(questions)
+      .then(({ name, atomicGroup, componentGroup }) => ({
+        directory: path.join(atomicGroup, componentGroup),
+        name,
+      }));
+  },
+};
