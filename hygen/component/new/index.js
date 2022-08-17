23bae6f465f97a43c761a16c2299653cd3b0488a
k.okayama <k.okayama@gemcook.com>
2022-08-17 21:43:42 +0900

    ROM-90 ディレクトリの整理

--- hygen/component/new/index.js
+++ hygen/component/new/index.js
@@ -1,27 +1,39 @@
-module.exports = {
-  prompt: ({ inquirer }) => {
-    const questions = [
-      {
-        message: `What's the component name?`,
-        name: 'name',
-        type: 'input',
-        validate: (input) => {
-          const isNull = input === '';
-          const isLowerCase = input.toLowerCase() === input;
-          return !isNull && !isLowerCase;
-        },
-      },
-      {
-        choices: ['parts', 'patterns', 'layouts'],
-        message: `What's the component group ?`,
-        name: 'group',
-        type: 'select',
-      },
-    ];
+import path from 'path';
 
-    return inquirer.prompt(questions).then(({ name, group }) => ({
-      directory: group,
+export function prompt({ inquirer }) {
+  const questions = [
+    {
+      message: `What's the component name?`,
+      name: 'name',
+      type: 'input',
+      validate: (input) => {
+        const isNull = input === '';
+        const isLowerCase = input.toLowerCase() === input;
+        return !isNull && !isLowerCase;
+      },
+    },
+    {
+      choices: ['parts', 'patterns', 'layouts'],
+      message: `What's the atomic group ?`,
+      name: 'atomicGroup',
+      type: 'select',
+    },
+    {
+      message: `What's the component group?`,
+      name: 'componentGroup',
+      type: 'input',
+      validate: (input) => {
+        const isNull = input === '';
+        const isLowerCase = input.toLowerCase() === input;
+        return isNull || !isLowerCase;
+      },
+    },
+  ];
+
+  return inquirer
+    .prompt(questions)
+    .then(({ name, atomicGroup, componentGroup }) => ({
+      directory: path.join(atomicGroup, componentGroup),
       name,
     }));
-  },
-};
+}
