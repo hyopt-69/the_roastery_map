12e85a46953de1b69dfd9f46a9e162d006a54479
k.okayama <k.okayama@gemcook.com>
2022-10-02 22:25:07 +0900

    ROM-71 ActionButtonコンポーネントの実装

---
+++ src/components/parts/Button/ActionButton/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { IconPattern } from '@/app/assets/icons';
+import { Icon } from '@/components/parts/Image/Icon';
+
+import { getStyles, StyleProps } from './styles';
+
+type ButtonProps = Pick<React.ComponentProps<'button'>, 'onClick'>;
+type ActionPattern = 'close' | 'next' | 'back';
+type Props = {
+  pattern: ActionPattern;
+} & StyleProps &
+  ButtonProps;
+
+const ICON: Record<ActionPattern, IconPattern> = {
+  close: 'X',
+  next: 'CHEVRON_RIGHT',
+  back: 'CHEVRON_LEFT',
+};
+
+export const ActionButton: React.FC<Props> = ({
+  pattern,
+  size = 'm',
+  onClick,
+}) => {
+  const styles = getStyles({ size });
+
+  return (
+    <button type="button" css={styles.container} onClick={onClick}>
+      <Icon pattern={ICON[pattern]} size={size} stroke="mirage" />
+    </button>
+  );
+};
---
+++ src/stories/components/parts/Button/ActionButton/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+import { ActionButton } from '@/components/parts/Button/ActionButton';
+
+export default {
+  component: ActionButton,
+} as ComponentMeta<typeof ActionButton>;
+
+export const Index: ComponentStoryObj<typeof ActionButton> = {
+  args: {
+    size: 'm',
+    pattern: 'close',
+  },
+};
