1b755ea7cc5b5b8ddb8570fd4e3df38f77a35a0a
k.okayama <k.okayama@gemcook.com>
2022-10-10 11:21:40 +0900

    ROM-121 IconをRemixIceのものに差し替える。

--- src/components/parts/ActionButton/index.tsx
+++ src/components/parts/ActionButton/index.tsx
@@ -18,8 +18,8 @@ type Props = {
   ButtonProps;
 
 const ICON: Record<ActionPattern, IconPattern> = {
-  next: 'CHEVRON_RIGHT',
-  back: 'CHEVRON_LEFT',
+  next: 'ARROW_RIGHT',
+  back: 'ARROW_LEFT',
 };
 
 export const ActionButton: React.FC<Props> = ({
